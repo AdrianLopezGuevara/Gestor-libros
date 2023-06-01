9e4d4d7d5559ae0b2a7950eb0635da1387457abc
HurkiCorgi <al0826503@gmail.com>
2023-06-01 10:33:41 -0600

    Funcionando

---
+++ src/includes/get.php
@@ -0,0 +1,14 @@
+<?php
+
+$query = "SELECT * FROM libros;";
+
+require './config/connection.php';
+$request = mysqli_query($connection, $query);
+if ($request) {
+
+    $data = mysqli_fetch_array($request);
+    $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+    echo ($json);
+} else {
+    echo json_encode("Error");
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     eventListeners();
+    fetchLibro();
 });
 
 function eventListeners() {
@@ -12,6 +13,57 @@ function navegacionResponsive() {
     navegacion.classList.toggle('mostrar');
 }
 
-/* function (params) {
-    
-} */
+function fetchLibro() {
+    fetch('../src/includes/get.php', {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(libro => {
+            /* for (let i = 0; i < libro.length; i++) {
+                const portada = libro['portada'];
+                const titulo = libro['titulo'];
+                const autor = libro['autor'];
+                const fecha = libro['fecha'];
+
+                addLibro(portada, titulo, autor, fecha)
+            } */
+            const portada = libro['portada'];
+            const titulo = libro['titulo'];
+            const autor = libro['autor'];
+            const fecha = libro['fecha'];
+
+            addLibro(portada, titulo, autor, fecha);
+        })
+}
+
+function addLibro(_portada, _titulo, _autor, _fecha) {
+    const urlPortada = "../../portadas/" + _portada;
+    const libreria = document.querySelector('.libary');
+
+    const libro = document.createElement('div');
+    libro.classList.add('libro');
+
+    const link = document.createElement('a');
+    link.href = "./libro.html";
+
+    const portada = document.createElement('img');
+    portada.src = urlPortada;
+
+    const titulo = document.createElement('h3');
+    titulo.textContent = _titulo;
+
+    const autor = document.createElement('p');
+    autor.classList.add('autor');
+    autor.textContent = _autor;
+
+    const fecha = document.createElement('p');
+    fecha.classList.add('fecha');
+    fecha.textContent = _fecha;
+
+    libreria.appendChild(libro);
+    libro.appendChild(link);
+    link.appendChild(portada);
+    libro.appendChild(titulo);
+    libro.appendChild(autor);
+    libro.appendChild(fecha);
+}
